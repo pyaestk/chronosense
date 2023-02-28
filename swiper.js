8694ba1b56003f2302f82882edd88a18bc5ec8a2
LyNNxMooon <112456534+LyNNxMooon@users.noreply.github.com>
2023-02-28 14:14:51 +0630

    klt js

--- swiper.js
+++ swiper.js
@@ -24,3 +24,41 @@ var swiper = new Swiper(".slide-content", {
       },
   },
 });
+
+
+//kyaw lin thant
+
+function realtimeClock() {
+  var rtClock = new Date();
+
+  var hours = rtClock.getHours();
+  var minutes = rtClock.getMinutes();
+  var seconds = rtClock.getSeconds();
+
+  var amPm = (hours < 12) ? "AM" : "PM";
+
+  hours = (hours > 12) ? hours - 12 : hours;
+
+  hours = ("0" + hours).slice(-2);
+  minutes = ("0" + minutes).slice(-2);
+  seconds = ("0" + seconds).slice(-2);
+
+  document.getElementById('clock').innerHTML =
+      hours + "  :  " + minutes + "  :  " + seconds + "   " + amPm;
+  var t = setTimeout(realtimeClock, 500);
+}
+
+let n;
+
+document.getElementById("cal1").onclick = function () {
+  n = document.getElementById("username").value;
+  const c = document.getElementById("rda");
+  const f = document.getElementById("rda1");
+  if (n < 0 || n > 0) {
+      window.alert("Numbers are not accepted. Enter only words!");
+  } else if (c.checked) {
+      document.getElementById("user").innerHTML = "Mr." + n;
+  } else if (f.checked) {
+      document.getElementById("user").innerHTML = "Mrs." + n;
+  }
+}
