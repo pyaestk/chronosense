9971a5b6da2f1429d6b54e73a144d85029ae6546
pyaestk <98664956+pyaestk@users.noreply.github.com>
2023-03-01 21:01:02 +0630

    update

--- swiper.js
+++ swiper.js
@@ -52,12 +52,10 @@ var swiper = new Swiper(".slide-content2", {
 });
 
 var swiper = new Swiper(".mySwiper", {
+  speed: 600,
+  parallax: true,
   loop: true,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
